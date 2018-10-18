90e85b05468d588c97852f90ab667dd57d47b7f3
Nicolas Hodicq <>
2018-10-19 00:30:45 +0200

    version v0.0.10 v0.0.11 v0.0.12 v0.0.13 v0.0.14 v0.0.15 v0.0.16 v0.0.17 v0.0.18 v0.0.19 v0.0.20 v0.0.21 v0.0.22 v0.0.23 v0.0.24 v0.0.6 v0.0.7 v0.0.8 v0.0.9 v0.1.0 v0.1.1 v0.1.2 v0.1.3 v0.1.4 v0.1.5 v0.1.6 v0.1.7 v0.1.8 v0.1.9 v0.10.0 v0.2.0 v0.2.1 v0.2.10 v0.2.11 v0.2.2 v0.2.3 v0.2.4 v0.2.5 v0.2.6 v0.2.7 v0.2.8 v0.2.9 v0.3.0 v0.3.1 v0.3.2 v0.3.3 v0.3.4 v0.3.5 v0.3.6 v0.4.0 v0.4.1 v0.4.2 v0.4.3 v0.4.4 v0.5.0 v0.5.1 v0.5.2 v0.5.3 v0.5.4 v0.5.5 v0.5.6 v0.5.7 v0.5.8 v0.6.0 v0.6.1 v0.6.2 v0.7.0 v0.7.1 v0.7.2 v0.7.3 v0.7.4 v0.7.5 v0.8.0 v0.8.1 v0.8.2 v0.8.3 v0.8.4 v0.8.5 v0.8.6 v0.8.7 v0.9.0 v0.9.1 v0.9.2 v0.9.3 v0.9.4 v0.9.5 v0.9.6

---
+++ README.md
@@ -0,0 +1,20 @@
+# Flutter Cli diff
+
+This repository exposes an Flutter CLI app generated with
+`flutter create flutterdiff --no-pub --org nartawak -i objc -a java` with each version of Flutter CLI.
+
+A dedicated branch per version makes changes very easy
+to watch. For example:
+
+* https://github.com/nartawak/flutter-cli-diff/compare/v0.0.10...v0.0.11
+* https://github.com/nartawak/flutter-cli-diff/compare/v0.1.3...v0.3.1
+
+See table below for the complete list.
+
+Note that this is inspired by [Angular Cli diff](https://github.com/cexbrayat/angular-cli-diff) & [RN diff](https://github.com/ncuillery/rn-diff).
+
+## Version changes
+
+Version|Compare view|Stats
+----|----|----
+    NEWLINE
---
+++ new-version.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+# Keep the current directory
+currentDir="$(pwd)"
+
+#FlutterDir
+flutterDir="$currentDir/flutter"
+
+# Clone Flutter repository
+rm -Rf "$flutterDir"
+git clone git@github.com:flutter/flutter.git
+
+# Move to the flutter directory and get all tags (format is vx.x.x)
+cd "$flutterDir"
+versions=("$(git tag -l)")
+cd "$currentDir"
+
+lastVersion="v0.0.10"
+rebaseNeeded=false
+
+for version in "${versions[@]}"; do
+
+    if [ `git branch --list ${version}` ] || [ `git branch --list --remote origin/${version}` ]
+    then
+        echo "${version} already generated."
+        git checkout ${version}
+
+        if [ ${rebaseNeeded} = true ]
+        then
+            git rebase --onto ${lastVersion} head~ ${version} -X theirs
+            diffStat=`git --no-pager diff head~ --shortstat`
+            #git push origin ${version} -f
+            diffUrl="[${lastVersion}...${version}](https://github.com/nartawak/flutter-cli-diff/compare/${lastVersion}...${version})"
+            git checkout master
+            # rewrite stats in README after rebase
+            sed -i "" "/^${version}|/ d" README.md
+            sed -i '' 's/----|----|----/----|----|----\
+            NEWLINE/g' README.md
+            sed -i "" "s@NEWLINE@${version}|${diffUrl}|${diffStat}@" README.md
+            git commit -a --amend --no-edit
+            git checkout ${version}
+        fi
+
+        lastVersion=${version}
+        continue
+    fi
+
+    echo "Generate ${version}"
+    rebaseNeeded=true
+    git checkout -b ${version}
+
+    # delete app
+    rm -rf flutterdiff
+
+    # Install flutter version
+    cd "$flutterDir"
+    git checkout ${version}
+    cd "$currentDir"
+
+    # Generate Flutter base project with default langage value
+    ./flutter/bin/flutter create flutterdiff --no-pub --org nartawak -i objc -a java
+
+    git add flutterdiff
+    git commit -am "version ${version}"
+    diffStat=`git --no-pager diff head~ --shortstat`
+    git push origin ${version} -f
+
+    git checkout master
+    diffUrl="[${lastVersion}...${version}](https://github.com/cexbrayat/angular-cli-diff/compare/${lastVersion}...${version})"
+    # insert a row in the version table of the README
+    sed -i "" "/^${version}|/ d" README.md
+    sed -i '' 's/----|----|----/----|----|----\
+    NEWLINE/g' README.md
+    sed -i "" "s@NEWLINE@${version}|${diffUrl}|${diffStat}@" README.md
+    # commit
+    git commit -a --amend --no-edit
+    git checkout ${version}
+    lastVersion=${version}
+
+done
+
+git checkout master
+#git push origin master -f
